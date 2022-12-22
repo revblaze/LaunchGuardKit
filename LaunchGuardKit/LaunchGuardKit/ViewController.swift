24d449bec6edb20f965d746a7f4936443ee27937
Justin Bush <revblaze@users.noreply.github.com>
2022-12-22 10:29:51 -0700

    feat: demo project

---
+++ LaunchGuardKit/LaunchGuardKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LaunchGuardKit
+//
+//  Created by Justin Bush on 12/22/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
