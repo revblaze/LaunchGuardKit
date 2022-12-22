9124406c38984e938377233e4d0a2428d3db3144
Justin Bush <revblaze@users.noreply.github.com>
2022-12-22 15:39:36 -0700

    refactor: runningApps → apps

--- LaunchGuardKit/LaunchGuardKit/LaunchGuardKit/LaunchGuard.swift
+++ LaunchGuardKit/LaunchGuardKit/LaunchGuardKit/LaunchGuard.swift
@@ -51,69 +51,10 @@ extension LaunchGuard {
     return workspace.runningApplications
   }
   
-  public func runningAppNames() -> [String] {
-    var names: [String] = []
-    for app in runningApplications() {
-      if let name = app.localizedName {
-        names.append(name)
-      }
-    }
-    return names
-  }
-  
-  public func runningBundleIds() -> [String] {
-    var ids: [String] = []
-    for app in runningApplications() {
-      if let bundleId = app.bundleIdentifier {
-        ids.append(bundleId)
-      }
-    }
-    return ids
-  }
-  
-  public func runningAppNamesAndBundleIds() -> [String] {
-    var ids: [String] = []
-    let runningApps = runningApplications()
-    
-    for app in runningApps {
-      if let name = app.localizedName {
-        ids.append(name)
-      }
-      if let bundleId = app.bundleIdentifier {
-        ids.append(bundleId)
-      }
-    }
-    
-    return ids
-  }
-  
   
   
   // MARK: - Recently Launched
   
-  public func recentlyLaunchedAppName() -> String {
-    if let name = runningAppNames().last {
-      return name
-    }
-    return "N/A"
-  }
   
-  public func recentlyLaunchedBundleId() -> String {
-    if let bundleId = runningBundleIds().last {
-      return bundleId
-    }
-    return "N/A"
-  }
-  
-  public func recentlyLaunched() -> String {
-    var recentlyLaunched = ""
-    if let name = runningAppNames().last {
-      recentlyLaunched = name
-    }
-    if let bundleId = runningBundleIds().last {
-      recentlyLaunched.append(" [\(bundleId)]")
-    }
-    return recentlyLaunched
-  }
   
 }
